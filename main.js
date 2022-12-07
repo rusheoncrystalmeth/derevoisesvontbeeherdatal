d5862dcc5e2bbcb0578f8a067aa2eca39b3bf698
rusheoncrystalmeth <102523000+rusheoncrystalmeth@users.noreply.github.com>
2022-12-07 13:49:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+noseX=0;
+noseY=0;
+
+function preload()
+{
+  clown_nose = loadImage('https://i.postimg.cc/LXt0ssPn/Screenshot-20221207-131551.png');
+}
+
+function setup()
+{
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('Posenet Is Initialized');
+}
+
+function draw()
+{
+    image(video, 0, 0, 320, 280);
+    fill(255, 0, 0);
+    stroke(255, 0, 0);
+    image(clown_nose, noseX, noseY, 30, 30);
+}
+
+function take_snapshot()
+{
+    save('myFilterImage.png');
+}
+
+function gotPoses(results)
+{
+  if (results.length > 0)
+  {
+    console.log(results);
+    noseX = results[0].pose.nose.x -5;
+    noseY = results[0].pose.nose.y -5;
+    console.log("nose x = " + noseX);
+    console.log("nose y = " + noseY);
+  }
+}
